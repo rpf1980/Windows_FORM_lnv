c837f5cba8740a4266958d40f1bc0d22a012bd13
rpf1980 <united_ssh@outlook.com>
2019-11-11 12:38:14 +0100

    feat():(pcClase) add new project --> BancoFicheroXML

---
+++ BancoFicherosXML/BancoFicherosXML/Banco.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancoFicherosXML
+{
+    class Banco
+    {
+        //Atributos
+        List<Cliente> listaClientes = new List<Cliente>();
+
+        //Constructores
+        public Banco() {}
+
+        public Banco(List<Cliente> listaClientes)
+        {
+            this.ListaClientes = listaClientes;
+        }
+
+        //Getter && Setter
+        internal List<Cliente> ListaClientes { get => listaClientes; set => listaClientes = value; }
+        
+        //Métodos
+        public void AddCliente(Cliente cliente)
+        {
+            this.listaClientes.Add(cliente);
+        }
+
+        /*private string dni;
+        private string nombre;
+        private string direccion;
+        private int edad;
+        private int tlfn;
+        private int cc;*/
+
+        public Cliente ModificarCliente(string dni)
+        {
+            Cliente cli = null;
+            for (int i = 0; i < this.listaClientes.Count; i++)
+            {
+                string docDni = this.listaClientes[i].Dni;
+
+                if(docDni.Equals(dni))
+                {
+                    cli = this.listaClientes[i];
+                }
+            }
+            return cli;
+        }
+        
+        public void EliminaCliente(Cliente cliente)
+        {
+            for (int i = 0; i < this.listaClientes.Count; i++)
+            {
+                if(this.listaClientes[i] == cliente)
+                {
+                    this.listaClientes.RemoveAt(i);
+                }
+            }
+        }
+    }
+}
---
+++ BancoFicherosXML/BancoFicherosXML/Cliente.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancoFicherosXML
+{
+    class Cliente
+    {
+        //Atributos
+        private string dni;
+        private string nombre;
+        private string direccion;
+        private int edad;
+        private int tlfn;
+        private int cc;
+
+        //Constructores
+        public Cliente() { }
+
+        public Cliente(string dni, string nombre, string direccion, int edad, int tlfn, int cc)
+        {
+            this.dni = dni;
+            this.nombre = nombre;
+            this.direccion = direccion;
+            this.edad = edad;
+            this.tlfn = tlfn;
+            this.cc = cc;
+        }
+
+        public string Dni { get => dni; set => dni = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Direccion { get => direccion; set => direccion = value; }
+        public int Edad { get => edad; set => edad = value; }
+        public int Tlfn { get => tlfn; set => tlfn = value; }
+        public int Cc { get => cc; set => cc = value; }
+    }
+}
