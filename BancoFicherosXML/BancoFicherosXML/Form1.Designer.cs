49a9c3ede61a7c6b05b4c7f85fc1d46a0f673e59
rpf1980 <united_ssh@outlook.com>
2019-11-11 12:53:23 +0100

    chore(): add buttons gui

--- BancoFicherosXML/BancoFicherosXML/Form1.Designer.cs
+++ BancoFicherosXML/BancoFicherosXML/Form1.Designer.cs
@@ -28,13 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(39, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(179, 49);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "AÑADIR CLIENTES";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(39, 90);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(179, 49);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "MODIFICAR DATOS CLIENTES";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(39, 149);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(179, 49);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "ELIMINAR CLIENTES";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.button4.ForeColor = System.Drawing.Color.White;
+            this.button4.Location = new System.Drawing.Point(39, 208);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(179, 49);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "MOSTRAR DATOS CLIENTES";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackgroundImage = global::BancoFicherosXML.Properties.Resources.cambiar;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(526, 560);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.DoubleBuffered = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GESTION BANCO";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
