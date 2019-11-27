92f9bc871a92b9275e318b60587f53092877acb8
rpf1980 <united_ssh@outlook.com>
2019-11-27 13:44:12 +0100

    chore(class): subimos github

---
+++ ExploradordeArchivos/ExploradordeArchivos/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace ExploradordeArchivos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStripContainer1 = new System.Windows.Forms.ToolStripContainer();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.imgListSmall = new System.Windows.Forms.ImageList(this.components);
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.imgListBig = new System.Windows.Forms.ImageList(this.components);
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.vISTASToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.idIconosPequenios = new System.Windows.Forms.ToolStripMenuItem();
+            this.idIconosGrandes = new System.Windows.Forms.ToolStripMenuItem();
+            this.idLista = new System.Windows.Forms.ToolStripMenuItem();
+            this.idDetalle = new System.Windows.Forms.ToolStripMenuItem();
+            this.sALIRToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.imgLisBig = new System.Windows.Forms.ImageList(this.components);
+            this.toolStripContainer1.ContentPanel.SuspendLayout();
+            this.toolStripContainer1.TopToolStripPanel.SuspendLayout();
+            this.toolStripContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.menuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStripContainer1
+            // 
+            // 
+            // toolStripContainer1.ContentPanel
+            // 
+            this.toolStripContainer1.ContentPanel.Controls.Add(this.splitContainer1);
+            this.toolStripContainer1.ContentPanel.Size = new System.Drawing.Size(771, 472);
+            this.toolStripContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.toolStripContainer1.LeftToolStripPanelVisible = false;
+            this.toolStripContainer1.Location = new System.Drawing.Point(0, 0);
+            this.toolStripContainer1.Name = "toolStripContainer1";
+            this.toolStripContainer1.RightToolStripPanelVisible = false;
+            this.toolStripContainer1.Size = new System.Drawing.Size(771, 496);
+            this.toolStripContainer1.TabIndex = 0;
+            this.toolStripContainer1.Text = "toolStripContainer1";
+            // 
+            // toolStripContainer1.TopToolStripPanel
+            // 
+            this.toolStripContainer1.TopToolStripPanel.Controls.Add(this.menuStrip);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.treeView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.listView1);
+            this.splitContainer1.Size = new System.Drawing.Size(771, 472);
+            this.splitContainer1.SplitterDistance = 256;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.ImageIndex = 0;
+            this.treeView1.ImageList = this.imgListSmall;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.SelectedImageIndex = 0;
+            this.treeView1.Size = new System.Drawing.Size(256, 472);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView1_NodeMouseClick);
+            // 
+            // imgListSmall
+            // 
+            this.imgListSmall.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imgListSmall.ImageStream")));
+            this.imgListSmall.TransparentColor = System.Drawing.Color.Transparent;
+            this.imgListSmall.Images.SetKeyName(0, "carpeta.png");
+            this.imgListSmall.Images.SetKeyName(1, "exe.png");
+            this.imgListSmall.Images.SetKeyName(2, "foto.png");
+            this.imgListSmall.Images.SetKeyName(3, "iso.png");
+            this.imgListSmall.Images.SetKeyName(4, "otros.png");
+            this.imgListSmall.Images.SetKeyName(5, "pdf.png");
+            this.imgListSmall.Images.SetKeyName(6, "rar.png");
+            this.imgListSmall.Images.SetKeyName(7, "txt.png");
+            this.imgListSmall.Images.SetKeyName(8, "binary.png");
+            this.imgListSmall.Images.SetKeyName(9, "mp3.png");
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.HideSelection = false;
+            this.listView1.LargeImageList = this.imgListBig;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(511, 472);
+            this.listView1.SmallImageList = this.imgListSmall;
+            this.listView1.StateImageList = this.imgListSmall;
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Nombre";
+            this.columnHeader1.Width = 300;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Tipo";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Última Modificación";
+            // 
+            // imgListBig
+            // 
+            this.imgListBig.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imgListBig.ImageStream")));
+            this.imgListBig.TransparentColor = System.Drawing.Color.Transparent;
+            this.imgListBig.Images.SetKeyName(0, "carpeta.ico");
+            this.imgListBig.Images.SetKeyName(1, "exe.ico");
+            this.imgListBig.Images.SetKeyName(2, "foto.ico");
+            this.imgListBig.Images.SetKeyName(3, "iso.ico");
+            this.imgListBig.Images.SetKeyName(4, "otros.ico");
+            this.imgListBig.Images.SetKeyName(5, "pdf.ico");
+            this.imgListBig.Images.SetKeyName(6, "rar.ico");
+            this.imgListBig.Images.SetKeyName(7, "txt.ico");
+            this.imgListBig.Images.SetKeyName(8, "binary.png");
+            this.imgListBig.Images.SetKeyName(9, "mp3.png");
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Dock = System.Windows.Forms.DockStyle.None;
+            this.menuStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.vISTASToolStripMenuItem,
+            this.sALIRToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(771, 24);
+            this.menuStrip.TabIndex = 0;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // vISTASToolStripMenuItem
+            // 
+            this.vISTASToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.idIconosPequenios,
+            this.idIconosGrandes,
+            this.idLista,
+            this.idDetalle});
+            this.vISTASToolStripMenuItem.Name = "vISTASToolStripMenuItem";
+            this.vISTASToolStripMenuItem.Size = new System.Drawing.Size(54, 20);
+            this.vISTASToolStripMenuItem.Text = "VISTAS";
+            // 
+            // idIconosPequenios
+            // 
+            this.idIconosPequenios.Name = "idIconosPequenios";
+            this.idIconosPequenios.Size = new System.Drawing.Size(164, 22);
+            this.idIconosPequenios.Text = "Iconos pequeños";
+            this.idIconosPequenios.Click += new System.EventHandler(this.idIconosPequenios_Click);
+            // 
+            // idIconosGrandes
+            // 
+            this.idIconosGrandes.Name = "idIconosGrandes";
+            this.idIconosGrandes.Size = new System.Drawing.Size(164, 22);
+            this.idIconosGrandes.Text = "Iconos grandes";
+            this.idIconosGrandes.Click += new System.EventHandler(this.idIconosGrandes_Click);
+            // 
+            // idLista
+            // 
+            this.idLista.Name = "idLista";
+            this.idLista.Size = new System.Drawing.Size(164, 22);
+            this.idLista.Text = "Lista";
+            this.idLista.Click += new System.EventHandler(this.idLista_Click);
+            // 
+            // idDetalle
+            // 
+            this.idDetalle.Name = "idDetalle";
+            this.idDetalle.Size = new System.Drawing.Size(164, 22);
+            this.idDetalle.Text = "Detalle";
+            this.idDetalle.Click += new System.EventHandler(this.idDetalle_Click);
+            // 
+            // sALIRToolStripMenuItem
+            // 
+            this.sALIRToolStripMenuItem.Name = "sALIRToolStripMenuItem";
+            this.sALIRToolStripMenuItem.Size = new System.Drawing.Size(49, 20);
+            this.sALIRToolStripMenuItem.Text = "SALIR";
+            this.sALIRToolStripMenuItem.Click += new System.EventHandler(this.sALIRToolStripMenuItem_Click);
+            // 
+            // imgLisBig
+            // 
+            this.imgLisBig.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imgLisBig.ImageSize = new System.Drawing.Size(16, 16);
+            this.imgLisBig.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(771, 496);
+            this.Controls.Add(this.toolStripContainer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "Form1";
+            this.Text = "EXPLORADOR DE ARCHIVOS";
+            this.toolStripContainer1.ContentPanel.ResumeLayout(false);
+            this.toolStripContainer1.TopToolStripPanel.ResumeLayout(false);
+            this.toolStripContainer1.TopToolStripPanel.PerformLayout();
+            this.toolStripContainer1.ResumeLayout(false);
+            this.toolStripContainer1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStripContainer toolStripContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ImageList imgListSmall;
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem vISTASToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem idIconosPequenios;
+        private System.Windows.Forms.ToolStripMenuItem idIconosGrandes;
+        private System.Windows.Forms.ToolStripMenuItem sALIRToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem idLista;
+        private System.Windows.Forms.ToolStripMenuItem idDetalle;
+        private System.Windows.Forms.ImageList imgLisBig;
+        private System.Windows.Forms.ImageList imgListBig;
+    }
+}
+
---
+++ ExploradordeArchivos/ExploradordeArchivos/Form1.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ExploradordeArchivos
+{
+    public partial class Form1 : Form
+    {     
+        public Form1()
+        {
+            InitializeComponent();
+            PopulateTreeView();
+            
+        }
+        private void PopulateTreeView()
+        {
+            TreeNode rootNode;         
+            DirectoryInfo info = new DirectoryInfo(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments));
+
+            try
+            {
+                if (info.Exists)
+                {
+                    rootNode = new TreeNode(info.Name);
+                    rootNode.Tag = info;
+                    GetDirectories(info.GetDirectories(), rootNode);
+                    treeView1.Nodes.Add(rootNode);
+                }
+            }
+            catch(IOException ex)
+            {
+                MessageBox.Show(ex.Message);      
+            }           
+        }
+        private void GetDirectories(DirectoryInfo[] subDirs, TreeNode nodeToAddTo)
+        {
+            try
+            {
+                TreeNode aNode;
+                DirectoryInfo[] subSubDirs;
+                foreach (DirectoryInfo subDir in subDirs)
+                {
+                    try
+                    {
+                        aNode = new TreeNode(subDir.Name, 0, 0);
+                        aNode.Tag = subDir;
+                        aNode.ImageKey = "Folder";
+                        subSubDirs = subDir.GetDirectories();
+                        if (subSubDirs.Length != 0)
+                        {
+                            GetDirectories(subSubDirs, aNode);
+                        }
+                        nodeToAddTo.Nodes.Add(aNode);
+                    }
+                    catch(UnauthorizedAccessException ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }                    
+                }
+            }
+            catch(IOException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+        private void treeView1_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            TreeNode newSelected = e.Node;
+            listView1.Items.Clear();
+            DirectoryInfo nodeDirInfo = (DirectoryInfo)newSelected.Tag;
+            
+            ListViewItem.ListViewSubItem[] subItems;
+            ListViewItem item = null;
+            foreach (DirectoryInfo dir in nodeDirInfo.GetDirectories())
+            {
+                item = new ListViewItem(dir.Name, 0);
+                subItems = new ListViewItem.ListViewSubItem[]
+                {new ListViewItem.ListViewSubItem(item, "Directorio"),new ListViewItem.ListViewSubItem(item,dir.LastAccessTime.ToShortDateString())};
+                item.SubItems.AddRange(subItems);
+                listView1.Items.Add(item);
+            }
+            foreach (FileInfo file in nodeDirInfo.GetFiles())
+            {
+                //Switch que usamos para validar tipo de extensión
+                switch (file.Extension)
+                {
+                    case ".txt":
+                        {
+                            item = new ListViewItem(file.Name, 7); //Item con índice 7 (Ficheros)
+                            subItems = new ListViewItem.ListViewSubItem[] //Array de subitems
+                            { new ListViewItem.ListViewSubItem(item, "Fichero"),//Esta línea establece texto a mostrar del item
+                              new ListViewItem.ListViewSubItem(item,file.LastAccessTime.ToShortDateString())}; //Esta línea correponde a la info del último acceso al fichero
+                            item.SubItems.AddRange(subItems); //Añade los subitems
+                            listView1.Items.Add(item); //Añade el item completo a la listview
+                        }
+                        break;
+                    case ".exe":
+                        {
+                            item = new ListViewItem(file.Name, 1); //Item con índice 1 (Archivos ejecutables)
+                            subItems = new ListViewItem.ListViewSubItem[] //Array de subitems
+                            { new ListViewItem.ListViewSubItem(item, "Archivo ejecutable"),//Esta línea establece texto a mostrar del item
+                              new ListViewItem.ListViewSubItem(item,file.LastAccessTime.ToShortDateString())}; //Esta línea correponde a la info del último acceso al fichero
+                            item.SubItems.AddRange(subItems); //Añade los subitems
+                            listView1.Items.Add(item); //Añade el item completo a la listview
+                        }
+                        break;
+                    case ".png":
+                    case ".jpg":
+                    case ".gif":
+                    case ".ico":
+                        {
+                            item = new ListViewItem(file.Name, 2); //Item con índice 2 (Imágenes)
+                            subItems = new ListViewItem.ListViewSubItem[] //Array de subitems
+                            { new ListViewItem.ListViewSubItem(item, "Imagen"),//Esta línea establece texto a mostrar del item
+                              new ListViewItem.ListViewSubItem(item,file.LastAccessTime.ToShortDateString())}; //Esta línea correponde a la info del último acceso al fichero
+                            item.SubItems.AddRange(subItems); //Añade los subitems
+                            listView1.Items.Add(item); //Añade el item completo a la listview
+                        }
+                        break;
+                    case ".iso":
+                        {
+                            item = new ListViewItem(file.Name, 3); //Item con índice 2 (Imagen iso)
+                            subItems = new ListViewItem.ListViewSubItem[] //Array de subitems
+                            { new ListViewItem.ListViewSubItem(item, "Imagen iso"),//Esta línea establece texto a mostrar del item
+                              new ListViewItem.ListViewSubItem(item,file.LastAccessTime.ToShortDateString())}; //Esta línea correponde a la info del último acceso al fichero
+                            item.SubItems.AddRange(subItems); //Añade los subitems
+                            listView1.Items.Add(item); //Añade el item completo a la listview
+                        }
+                        break;
+                    case ".pdf":
+                        {
+                            item = new ListViewItem(file.Name, 5); //Item con índice 5 (Documentos pdf)
+                            subItems = new ListViewItem.ListViewSubItem[] //Array de subitems
+                            { new ListViewItem.ListViewSubItem(item, "Documento pdf"),//Esta línea establece texto a mostrar del item
+                              new ListViewItem.ListViewSubItem(item,file.LastAccessTime.ToShortDateString())}; //Esta línea correponde a la info del último acceso al fichero
+                            item.SubItems.AddRange(subItems); //Añade los subitems
+                            listView1.Items.Add(item); //Añade el item completo a la listview
+                        }
+                        break;
+                    case ".rar":
+                    case ".zip":
+                    case ".gz":
+                        {
+                            item = new ListViewItem(file.Name, 6); //Item con índice 6 (Archivo comprimido)
+                            subItems = new ListViewItem.ListViewSubItem[] //Array de subitems
+                            { new ListViewItem.ListViewSubItem(item, "Archivo comprimido"),//Esta línea establece texto a mostrar del item
+                              new ListViewItem.ListViewSubItem(item,file.LastAccessTime.ToShortDateString())}; //Esta línea correponde a la info del último acceso al fichero
+                            item.SubItems.AddRange(subItems); //Añade los subitems
+                            listView1.Items.Add(item); //Añade el item completo a la listview
+                        }
+                        break;
+                    case ".bin":
+                        {
+                            item = new ListViewItem(file.Name, 8); //Item con índice 8 (Archivos binarios)
+                            subItems = new ListViewItem.ListViewSubItem[] //Array de subitems
+                            { new ListViewItem.ListViewSubItem(item, "Archivo binario"),//Esta línea establece texto a mostrar del item
+                              new ListViewItem.ListViewSubItem(item,file.LastAccessTime.ToShortDateString())}; //Esta línea correponde a la info del último acceso al fichero
+                            item.SubItems.AddRange(subItems); //Añade los subitems
+                            listView1.Items.Add(item); //Añade el item completo a la listview
+                        }
+                        break;
+                    case ".mp3":
+                        {
+                            item = new ListViewItem(file.Name, 9); //Item con índice 9 (Archivos de audio mp3)
+                            subItems = new ListViewItem.ListViewSubItem[] //Array de subitems
+                            { new ListViewItem.ListViewSubItem(item, "Archivo mp3"),//Esta línea establece texto a mostrar del item
+                              new ListViewItem.ListViewSubItem(item,file.LastAccessTime.ToShortDateString())}; //Esta línea correponde a la info del último acceso al fichero
+                            item.SubItems.AddRange(subItems); //Añade los subitems
+                            listView1.Items.Add(item); //Añade el item completo a la listview
+                        }
+                        break;
+
+                    default:
+                        item = new ListViewItem(file.Name, 4); //Item con índice 4 (Otros archivos)
+                        subItems = new ListViewItem.ListViewSubItem[] //Array de subitems
+                        { new ListViewItem.ListViewSubItem(item, "Archivo"),//Esta línea establece texto a mostrar del item
+                              new ListViewItem.ListViewSubItem(item,file.LastAccessTime.ToShortDateString())}; //Esta línea correponde a la info del último acceso al fichero
+                        item.SubItems.AddRange(subItems); //Añade los subitems
+                        listView1.Items.Add(item); //Añade el item completo a la listview
+                        break;
+                }
+            }
+            listView1.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+        }
+
+        //Vista ICONOS PEQUEÑOS
+        private void idIconosPequenios_Click(object sender, EventArgs e)
+        {
+            listView1.View = View.SmallIcon;
+        }
+
+        //Vista ICONOS LISTA
+        private void idLista_Click(object sender, EventArgs e)
+        {
+            listView1.View = View.List;
+        }
+
+        //Vista ICONOS GRANDES
+        private void idIconosGrandes_Click(object sender, EventArgs e)
+        {
+            listView1.View = View.LargeIcon;
+        }
+
+        //Vista DETALLES
+        private void idDetalle_Click(object sender, EventArgs e)
+        {
+            listView1.View = View.Details;
+        }
+
+        //Btn SALIR 
+        private void sALIRToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
+
